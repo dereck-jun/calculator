a44995d5d6a11046b9e4b926e8e4cc5ad31e514e
dereck-jun <yjpark0805@naver.com>
2025-01-03 15:32:55 +0900

    fix: 예외 처리 추가 및 불필요한 캐스팅, 제어문 제거

--- src/main/java/com/example/lv1/App.java
+++ src/main/java/com/example/lv1/App.java
@@ -1,5 +1,6 @@
 package com.example.lv1;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
@@ -8,48 +9,47 @@ public class App {
         String loopStr = "";
 
         while (!loopStr.equalsIgnoreCase("exit")) {
-            System.out.print("첫 번째 양의 정수를 입력해주세요 > ");
-            int firstNum = scanner.nextInt();
-            if (!validateNumber(firstNum)) {
+            try {
+                System.out.print("첫 번째 0을 포함한 양의 정수를 입력해주세요 > ");
+                int firstNum = scanner.nextInt();
+                validateNumber(firstNum);
+
+                System.out.print("두 번째 0을 포함한 양의 정수를 입력해주세요 > ");
+                int secondNum = scanner.nextInt();
+
+                System.out.print("연산 기호를 입력해주세요 > ");
+                scanner.nextLine(); // 개행 문자 제거 용도
+                char operator = scanner.nextLine().charAt(0);
+
+                switch (operator) {
+                    case '+' -> System.out.println("덧셈 결과 > " + (firstNum + secondNum));
+                    case '-' -> System.out.println("뺄셈 결과 > " + (firstNum - secondNum));
+                    case '*' -> System.out.println("곱셈 결과 > " + (firstNum * secondNum));
+                    case '/' -> System.out.println("나눗셈 결과 > " + (double) firstNum / secondNum);
+                    default -> {
+                        System.out.println("지원하지 않는 연산 기호입니다.");
+                        continue;
+                    }
+                }
+            } catch (InputMismatchException ime) {
+                System.out.println("잘못된 입력입니다.");
+                scanner.nextLine();
+                continue;
+            } catch (IllegalArgumentException iae) {
+                System.out.println("0을 포함한 양의 정수만을 입력해야 합니다.");
+                scanner.nextLine();
                 continue;
             }
 
-            System.out.print("두 번째 양의 정수를 입력해주세요 > ");
-            int secondNum = scanner.nextInt();
-            if (!validateNumber(secondNum)) {
-                continue;
-            }
-
-            System.out.print("연산 기호를 입력해주세요 > ");
-            scanner.nextLine(); // 개행 문자 제거 용도
-            char operator = scanner.nextLine().charAt(0);
-
-            switch (operator) {
-                case '+':
-                    System.out.println("덧셈 결과 > " + ((double) firstNum + secondNum));
-                    break;
-                case '-':
-                    System.out.println("뺄셈 결과 > " + ((double) firstNum - secondNum));
-                    break;
-                case '*':
-                    System.out.println("곱셈 결과 > " + (double) firstNum * secondNum);
-                    break;
-                case '/':
-                    System.out.println("나눗셈 결과 > " + (double) firstNum / secondNum);
-                    break;
-                default:
-                    System.out.println("지원하지 않는 연산 기호입니다!");
-            }
             System.out.print("계속 진행하시겠습니까? (exit 입력 시 종료) > ");
             loopStr = scanner.nextLine();
         }
     }
 
-    private static boolean validateNumber(int num) {
-        if (num <= 0) {
-            System.out.println("양의 정수만을 입력해야 합니다!");
-            return false;
+    private static void validateNumber(int num) {
+        if (num >= 0) {
+            return;
         }
-        return true;
+        throw new IllegalArgumentException();
     }
 }
